649c2c160c983bc84501ca3ffecc7ff41c50d01a
bala-amypo <bala@amypo.in>
2025-12-25 18:47:46 +0000

    User commit

--- src/main/java/com/example/demo/controller/ClashRecordController.java
+++ src/main/java/com/example/demo/controller/ClashRecordController.java
@@ -2,6 +2,7 @@ package com.example.demo.controller;
 
 import com.example.demo.dto.ApiResponse;
 import com.example.demo.service.ClashDetectionService;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -15,8 +16,13 @@ public class ClashRecordController {
     }
 
     @GetMapping("/branch/{branchId}")
-    public ApiResponse getByBranch(@PathVariable Long branchId) {
-        return new ApiResponse(true, "Clashes fetched successfully",
-                service.detectClashes(branchId));
+    public ResponseEntity<ApiResponse> getByBranch(@PathVariable Long branchId) {
+        return ResponseEntity.ok(
+                new ApiResponse(
+                        true,
+                        "Clashes fetched successfully",
+                        service.detectClashes(branchId)
+                )
+        );
     }
 }
